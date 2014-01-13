deb4907e2ebdf8b9b456422a7ddf4de30f03c52c
Paul and Kyle <kburton@relaynetwork.com>
2014-01-13 15:46:47 -0500

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,8 +1,16 @@
-(defproject com.relaynetwork/irmgard "0.1.0"
+(defproject com.relaynetwork/irmgard "0.1.1-SNAPSHOT"
   :description "Irmgard: Postgres Event and Replication Framework for PostgreSQL"
+  :repositories [["releases"  {:url "s3p://relay-maven-repo/releases/"  :creds :gpg}]
+                 ["snapshots" {:url "s3p://relay-maven-repo/snapshots/" :creds :gpg}]
+                 ["sonatype"  {:url "https://oss.sonatype.org/content/groups/public/"}]]
   :url         "https://github.com/rn-superg/irmgard"
   :license     {:name "Eclipse Public License"
                 :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :lein-release {
+    :scm :git
+  }
+  :plugins      [[s3-wagon-private          "1.1.1"]
+                 [lein-release/lein-release "1.0.5"]]
   :resource-paths ["resources"]
   :profiles {:dev {:resource-paths ["dev-resources"]}}
   :dependencies [
