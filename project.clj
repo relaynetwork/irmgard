596ad0f6d8ef73be86d09651c8e17882451f503e
Josh and Rich <kburton@relaynetwork.com>
2014-01-20 15:38:30 -0500

    lein-release plugin: bumped version from 0.1.3 to 0.1.4-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/irmgard "0.1.3"
+(defproject com.relaynetwork/irmgard "0.1.4-SNAPSHOT"
   :description "Irmgard: Postgres Event and Replication Framework for PostgreSQL"
   :repositories [["releases"  {:url "s3p://relay-maven-repo/releases/"  :creds :gpg}]
                  ["snapshots" {:url "s3p://relay-maven-repo/snapshots/" :creds :gpg}]
